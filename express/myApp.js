3b11af89a7d5a099629b0b6ee26760a61b5a1c4b
ivan <block4ain226@gmail.com>
2024-11-07 15:07:26 +0200

    /json2

--- express/myApp.js
+++ express/myApp.js
@@ -1,4 +1,5 @@
 let express = require('express');
+require('dotenv').config()
 let app = express();
 
 // app.get('/', (req, res) => {
@@ -7,8 +8,11 @@ let app = express();
 
 app.use('/public', express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+app.get('/json', (req,res)=>{
+    const messageStyle = process.env.MESSAGE_STYLE
+    const message = 'Hello json'
+    const data =  {"message": messageStyle === 'uppercase' ? message.toUpperCase() : message}
+    res.json(data)
 })
 
 app.listen(3000);
