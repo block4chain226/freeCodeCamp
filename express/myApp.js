0dcb60b313d73ab19bb568fef81a715bc4ae79ed
ivan <block4ain226@gmail.com>
2024-11-07 18:40:22 +0200

    body-parser

--- express/myApp.js
+++ express/myApp.js
@@ -1,18 +1,15 @@
 let express = require('express');
+const {logger} = require('./middlewares/logger.middleware');
+const bodyParser = require('body-parser');
 const {timeMiddleware, timeRes} = require('./middlewares/time.middleware');
+const {getUser} = require('./handlers/users/user')
 require('dotenv').config()
 let app = express();
 
 
 app.use('/public', express.static(__dirname + '/public'))
-app.use((req, res, next) => {
-    const ip = req.ip
-    const path = req.path
-    const method = req.method
-    const info = `${method} ${path} - ${ip}`
-    console.log(info)
-    next()
-})
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(logger)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
@@ -26,9 +23,14 @@ app.get('/json', (req, res) => {
     res.json(data)
 })
 
+app.get('/now', timeMiddleware, timeRes)
+
 app.get('/:word/echo', (req, res) => {
     const word = req.params.word
     res.send({echo: word})
 })
 
-app.get('/now', timeMiddleware, timeRes)
+app.get('/name', getUser).post(getUser)
+
+
+module.exports = app;
